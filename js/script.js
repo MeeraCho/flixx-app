d67dcaf0dd3b72509172b1eac7c52dd42bfdc302
MiraCho <miracho119@gmail.com>
2024-03-15 22:07:59 -0600

    added movie details functionality

--- js/script.js
+++ js/script.js
@@ -50,13 +50,13 @@ async function displayPopularShows(){
                 show.poster_path
                 ? ` 
                 <img
-                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-                class="card-img-top"
-                alt="${show.name}"
-                />` : `<img
-                src="images/no-image.jpg"
-                class="card-img-top"
-                alt="${show.name}"
+                    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                    class="card-img-top"
+                    alt="${show.name}"
+                    />` : `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="${show.name}"
                 />`
             }
         </a>
@@ -70,17 +70,74 @@ async function displayPopularShows(){
     });
 }
 
+//-----------Display Movie Details--------------
+async function displayMovieDetails(){
+    const movieId = window.location.search;
+    console.log(window.location.search.split('=')[1]); //?id=792307
+
+    const movie = await fetchAPIData(`movie${movieId}`);
+
+    console.log(movie)
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+        <div class="details-top">
+            <div>
+                ${
+                    movie.poster_path
+                    ? `
+                    <img
+                        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                        class="card-img-top"
+                        alt="${movie.title}"
+                        />` : `<img
+                        src="images/no-image.jpg"
+                        class="card-img-top"
+                        alt="${movie.title}"
+                    />`
+                }
+            </div>
+            <div>
+                <h2>Movie Title</h2>
+                <p>
+                    <i class="fas fa-star text-primary"></i>
+                    ${movie.vote_average.toFixed(1)} / 10
+                </p>
+                <p class="text-muted">Release Date: ${movie.release_date}</p>
+                <p>${movie.overview}</p>
+                <h5>Genres</h5>
+                <ul class="list-group">
+                    ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+                </ul>
+                <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+            </div>
+        </div>
+        <div class="details-bottom">
+            <h2>Movie Info</h2>
+            <ul>
+                <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budge)}</li>
+                <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+                <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+                <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+            </ul>
+            <h4>Production Companies</h4>
+            <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join('')}</div>
+        </div>
+    `
+    document.querySelector('#movie-details').appendChild(div);
+}
+
 //-----------Fetch data from TMDB API--------------
 //url 'https://api.themoviedb.org/3/movie/11?api_key=ee71c49c9ede0d3c13c83a295fa3c70c'
 async function fetchAPIData(endpoint) {
-
+    
     const API_KEY = ''; 
     const API_URL = 'https://api.themoviedb.org/3';
 
     showSpinner();
 
     const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`)
-
+    
     const data = await response.json();
 
     hideSpinner();
@@ -108,8 +165,13 @@ function HightlightActiveLink() {
     }) 
 }
 
+//-------------Add Commas to Number---------------
+function addCommasToNumber(number){
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-//----------------Router------------------------
+
+//-------------Init App, Router-----------------
 function init(){
     switch(global.currentPage){
         case '/':
@@ -120,7 +182,7 @@ function init(){
             displayPopularShows()
             break;
         case '/movie-details.html': 
-            console.log('Movie Details');
+            displayMovieDetails()
             break;
         case '/tv-details.html': 
             console.log('TV Details');
