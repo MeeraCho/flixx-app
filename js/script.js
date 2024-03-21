502127b674346a4e005664d3ba528fa53c6985a3
MiraCho <miracho119@gmail.com>
2024-03-21 11:50:09 -0600

    added functionality of Display Search Results

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const global = {
         type: '',
         page: 1,
         totalPages: 1
+    },
+    api: {
+        // Only use this for development or very small projects. You should store your key and make requests from a server.
+        apiKEY : 'ee71c49c9ede0d3c13c83a295fa3c70c',
+        apiURL : 'https://api.themoviedb.org/3'
     }
 }
 
@@ -210,22 +215,59 @@ function displayBackgroundImage(type, backgroundPath){
 
 //-----------Search Movies/Shows--------------
 async function search(){
-    const queryString = window.location.search; 
-    console.log(queryString)
+    const queryString = window.location.search; //?type=movie&search-term=parasite - search창에 내용입력하면 나옴 
     const urlParams = new URLSearchParams(queryString);
-    console.log(urlParams)
+    // console.log(urlParams)
 
     global.search.type = urlParams.get('type');
     global.search.term = urlParams.get('search-term');
     
     if(global.search.term !== '' && global.search.term !== null){
-        const results = await searchAPIData();
-        console.log(results);
+        const { results, total_pages, page } = await searchAPIData();
+
+        if (results.length === 0) {
+            showAlert('No results found');
+            return;
+        }
+
+        displaySearchResults(results);
+
+        document.querySelector('#search-term').value = '';
+        
     } else {
-        showAlert('Please enter a search term');
+        showAlert('Please enter a search term', 'error');
     }
 }
 
+function displaySearchResults(results) {
+    results.forEach(result => {
+        console.log(result)
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${result.id}">
+                ${result.poster_path
+                    ? `
+                    <img
+                        src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+                        class="card-img-top"
+                        alt="${global.search.type === "movie" ? result.title: result.name}"
+                    />` : `<img
+                        src="images/no-image.jpg"
+                        class="card-img-top"
+                        alt="${global.search.type === "movie" ? result.title: result.name}"
+                    />`
+                }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${global.search.type === "movie" ? result.title: result.name}</h5>
+                <p class="card-text">
+                <small class="text-muted">Release: ${global.search.type === "movie" ? result.release_date: result.first_air_date}</small>
+                </p>
+            </div>`
+        document.querySelector('#search-results').appendChild(div);
+    });
+}
 
 //-----------Display Slider Movies--------------
 async function displaySlider(){
@@ -274,12 +316,26 @@ function initSwiper(){
 }
 
 
-
 //-----------Fetch data from TMDB API--------------
 //url 'https://api.themoviedb.org/3/movie/11?api_key=ee71c49c9ede0d3c13c83a295fa3c70c'
+async function fetchAPIData(endpoint) {
+    const API_KEY = global.api.apiKEY;
+    const API_URL = global.api.apiURL;
+
+    showSpinner();
+
+    const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`)
+
+    const data = await response.json();
+
+    hideSpinner();
+
+    return data;
+}
+
 async function fetchAPIDataWithAuth(endpoint) {
-    const API_KEY = '';
-    const API_URL = 'https://api.themoviedb.org/3';
+    const API_KEY = global.api.apiKEY;
+    const API_URL = global.api.apiURL;
     const API_Full_URL = `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
 
     // console.log(API_Full_URL);
@@ -302,15 +358,23 @@ async function fetchAPIDataWithAuth(endpoint) {
     return data;
 }
 
+async function searchAPIData() {
+    const API_KEY = global.api.apiKEY;
+    const API_URL = global.api.apiURL;
+    //'https://api.themoviedb.org/3/search/movie?query=parasite&include_adult=false&language=en-US&page=1'
+    const API_Full_URL = `${API_URL}/search/${global.search.type}?api_key=${API_KEY}&query=${global.search.term}&include_adult=false&language=en-US`
 
-async function fetchAPIData(endpoint) {
-
-    const API_KEY = 'ee71c49c9ede0d3c13c83a295fa3c70c';
-    const API_URL = 'https://api.themoviedb.org/3';
+    // console.log(API_Full_URL);
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+        }
+    };
 
     showSpinner();
 
-    const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`)
+    const response = await fetch(API_Full_URL, options)
 
     const data = await response.json();
 
@@ -318,6 +382,8 @@ async function fetchAPIData(endpoint) {
 
     return data;
 }
+
+
 
 //-----------------Show Spinner-------------------
 function showSpinner() {
@@ -340,7 +406,7 @@ function HightlightActiveLink() {
 }
 
 //-----------------Show Alert--------------------
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
     const alertEl = document.createElement('div');
     alertEl.classList.add('alert', className);
     alertEl.appendChild(document.createTextNode(message))
