7c49265a5d2081ab1ee8373e728fe9ef911065b6
MiraCho <miracho119@gmail.com>
2024-03-21 21:00:16 -0600

    added Pagination for Search

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
         term: '', //empty string to begin with
         type: '',
         page: 1,
-        totalPages: 1
+        totalPages: 1,
+        totalResults: 0
     },
     api: {
         // Only use this for development or very small projects. You should store your key and make requests from a server.
@@ -223,8 +224,13 @@ async function search(){
     global.search.term = urlParams.get('search-term');
     
     if(global.search.term !== '' && global.search.term !== null){
-        const { results, total_pages, page } = await searchAPIData();
+        const { results, total_pages, page, total_results } = await searchAPIData();
 
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
+
+        
         if (results.length === 0) {
             showAlert('No results found');
             return;
@@ -240,6 +246,11 @@ async function search(){
 }
 
 function displaySearchResults(results) {
+    //Clear previous results
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = ''; 
+
     results.forEach(result => {
         console.log(result)
         const div = document.createElement('div');
@@ -265,9 +276,52 @@ function displaySearchResults(results) {
                 <small class="text-muted">Release: ${global.search.type === "movie" ? result.release_date: result.first_air_date}</small>
                 </p>
             </div>`
+        document.querySelector('#search-results-heading').innerHTML = `
+                <h2>${results.length} of ${global.search.totalResults}
+                Results for "${global.search.term}"</h2>
+        `
         document.querySelector('#search-results').appendChild(div);
+
+    });
+    displayPagination();
+}
+
+// Create & Display Pagination for Search 
+function displayPagination(){
+    const div = document.createElement('div')
+    div.classList.add('pagination');
+    div.innerHTML = `
+        <button class="btn btn-primary" id="prev">Prev</button>
+        <button class="btn btn-primary" id="next">Next</button>
+        <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `
+    document.querySelector('#pagination').appendChild(div);
+
+    //Disable Prev button if on first page
+    if (global.search.page === 1) {
+        document.querySelector('#prev').disabled = true;
+    }
+
+    //Disable Next button if on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector('#next').disabled = true;
+    }
+
+    // Next page
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page++;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
+
+    // Prev page
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page--;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
     });
 }
+
 
 //-----------Display Slider Movies--------------
 async function displaySlider(){
@@ -362,7 +416,7 @@ async function searchAPIData() {
     const API_KEY = global.api.apiKEY;
     const API_URL = global.api.apiURL;
     //'https://api.themoviedb.org/3/search/movie?query=parasite&include_adult=false&language=en-US&page=1'
-    const API_Full_URL = `${API_URL}/search/${global.search.type}?api_key=${API_KEY}&query=${global.search.term}&include_adult=false&language=en-US`
+    const API_Full_URL = `${API_URL}/search/${global.search.type}?api_key=${API_KEY}&query=${global.search.term}&include_adult=false&language=en-US&page=${global.search.page}`
 
     // console.log(API_Full_URL);
     const options = {
